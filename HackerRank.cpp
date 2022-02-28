ae0b9c1304049827d349ae420b7e186c82495503
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-28 15:18:25 -0600

    more rank

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -77,44 +77,35 @@ string rtrim(const string &str) {
 }
 
 //========================================
+
+//if numbers between int a and int b are more than 9, print either even or odd.
+//if they are lower than 10, print their name in string.
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
-string number(int n) {
-    string names[10] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        if (n > 0 && n < 10) {
-        return names[n - 1];
-    } else if (n > 9) {
-        return "nine";
-    } else {
-        return "one";
-    }
-
-}
 string isEven(int n) {
     if(n % 2 == 0) {
-        return "even";
-    }
-    else {
-        return "odd";
+        return "even\n";
+    } else {
+        return "odd\n";
     }
 }
-
+string numberString(int n) {
+    string arrayNum [10] = {"one\n", "two\n", "three\n", "four\n", "five\n", "six\n", "seven\n", "eight\n", "nine\n"};
+    return arrayNum[n - 1];
+}
 int main() {
     // Complete the code.
-    int a;
-    int b;
-
+    int a, b;
     cin >> a >> b;
-    cout << number(a);
-    cout << "\n";
-    cout << number(b);
-    cout << "\n";
-    cout << isEven(a);
-    cout << "\n";
-    cout << isEven(b);
-
+    for(int i = a; i <= b; i++) {
+        if(i < 10) {
+            cout << numberString(i);
+        } else {
+            cout << isEven(i);
+        }
+    }
     return 0;
 }
 
@@ -165,5 +156,56 @@ int main() {
     int ans = max_of_four(a, b, c, d);
     printf("%d", ans);
 
+    return 0;
+}
+
+//=============================
+//find values using pointers
+#include <stdio.h>
+#include<cmath>
+void update(int *a,int *b) {
+    // Complete this function
+    int A = *a + *b;
+    int B = abs(*b - *a);
+
+    *a = A;
+    *b = B;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+//================================
+//dynamic array and reversing it.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N;
+    std::cin >> N;
+    int arr[N];
+    int num;
+    for(int i = N - 1; i >= 0; i--) {
+        cin >> num;
+        arr[i] = num;
+    }
+    for(int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
